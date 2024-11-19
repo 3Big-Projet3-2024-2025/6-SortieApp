86caf792d4e3c4ce596d0bb24db0bfd3203bd9e2
hus0w <la226628@student.helha.be>
2024-11-19 21:42:47 +0100

    Création du backend avec Spritng Boot

---
+++ SortieAppBackend/src/main/java/org/helha/be/sortieappbackend/SortieAppBackendApplication.java
@@ -0,0 +1,14 @@
+package org.helha.be.sortieappbackend;
+
+import org.helha.be.sortieappbackend.models.Adresse;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortieAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortieAppBackendApplication.class, args);
+    }
+
+}
---
+++ SortieAppBackend/src/test/java/org/helha/be/sortieappbackend/SortieAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.helha.be.sortieappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortieAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
