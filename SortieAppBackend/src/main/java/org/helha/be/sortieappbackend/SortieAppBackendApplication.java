4c0e7e58b905abc5d46f3ab8203351407daf8004
La215736-Gallet <la215736@student.helha.be>
2024-11-25 15:05:13 +0100

    added spring web dependency

--- SortieAppBackend/src/main/java/org/helha/be/sortieappbackend/SortieAppBackendApplication.java
+++ SortieAppBackend/src/main/java/org/helha/be/sortieappbackend/SortieAppBackendApplication.java
@@ -1,6 +1,5 @@
 package org.helha.be.sortieappbackend;
 
-import org.helha.be.sortieappbackend.models.Adresse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
