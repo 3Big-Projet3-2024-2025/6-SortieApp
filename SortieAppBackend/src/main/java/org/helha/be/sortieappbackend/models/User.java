b9fa8df0ef754a630f0ecc0e7a382243a09b4d33
la218256 <la218256@student.helha.be>
2024-12-10 13:51:20 +0100

    Modification User.java, Add relation for the table school

--- SortieAppBackend/src/main/java/org/helha/be/sortieappbackend/models/User.java
+++ SortieAppBackend/src/main/java/org/helha/be/sortieappbackend/models/User.java
@@ -10,7 +10,8 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int  id_user;
+    private int id_user;
+
     private String lastname_user;
     private String name_user;
     private String email_user;
@@ -18,19 +19,25 @@ public class User {
     private String address_user;
 
     @ManyToOne
-    @JoinColumn(name="role_id") // Create column 'role_id' in User table
-    @JsonIgnoreProperties("users") // Ignore users list in the role
+    @JoinColumn(name = "school_id") // Foreign key in the User table
+    @JsonIgnoreProperties("users_school") // Prevent infinite loops
+    private School school_user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id") // Foreign key for Role
+    @JsonIgnoreProperties("users")
     private Role role_user;
 
     public User() {}
 
-    public User(int id_user, String lastname_user, String name_user, String email_user, String password_user, String address_user, Role role_user) {
+    public User(int id_user, String lastname_user, String name_user, String email_user, String password_user, String address_user, School school_user, Role role_user) {
         this.id_user = id_user;
         this.lastname_user = lastname_user;
         this.name_user = name_user;
         this.email_user = email_user;
         this.password_user = password_user;
         this.address_user = address_user;
+        this.school_user = school_user;
         this.role_user = role_user;
     }
 }
