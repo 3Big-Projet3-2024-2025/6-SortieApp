2cfb3ffe622950ab12ee8fe8b5c7025bc24e5923
La215736 <la215736@student.helha.be>
2024-12-18 20:14:28 +0100

    created basic template page for the local_admin, supervisor and student home page

---
+++ SortieAppFrontend/sortie_app_frontend/lib/pages/student_home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class studentHomePage extends StatelessWidget {
+  final secureStorage = const FlutterSecureStorage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF87CEEB),
+          title: const Text('Home'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () async {
+                await secureStorage.deleteAll();
+                Get.offNamed('/login');
+              },
+            ),
+          ],
+        ),
+        body:
+        Center (
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+
+                },
+                child: const Text('QR code'),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
---
+++ SortieAppFrontend/sortie_app_frontend/lib/pages/supervisor_home_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class supervisorHomePage extends StatelessWidget {
+  final secureStorage = const FlutterSecureStorage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF87CEEB),
+          title: const Text('Home'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () async {
+                await secureStorage.deleteAll();
+                Get.offNamed('/login');
+              },
+            ),
+          ],
+        ),
+        body:
+        Center (
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Get.offNamed('/users');
+                },
+                child: const Text('User Management'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Get.offNamed('/schools');
+                },
+                child: const Text('Schools Management'),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Get.offNamed('/autorisations');
+                },
+                child: const Text('Autorisations management'),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
