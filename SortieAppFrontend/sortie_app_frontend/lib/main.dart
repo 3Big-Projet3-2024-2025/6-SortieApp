92a6feb435574c0b38d00a289991b69f3a9939ba
La215736 <la215736@student.helha.be>
2024-12-18 20:16:33 +0100

    changed name og the home page to adminHomePage

--- SortieAppFrontend/sortie_app_frontend/lib/main.dart
+++ SortieAppFrontend/sortie_app_frontend/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sortie_app_frontend/pages/login_page.dart';
-import 'package:sortie_app_frontend/pages/home_page.dart';
+import 'package:sortie_app_frontend/pages/admin_home_page.dart';
 import 'package:sortie_app_frontend/pages/user_crud_page.dart';
 import 'package:sortie_app_frontend/pages/schools_crud_page.dart';
 import 'package:sortie_app_frontend/pages/autorisation_crud_page.dart';
@@ -19,7 +19,7 @@ class SortieApp extends StatelessWidget {
       initialRoute: '/login',
       getPages: [
         GetPage(name: '/login', page: () => loginPage()),
-        GetPage(name: '/adminHome', page: () => homePage()),
+        GetPage(name: '/adminHome', page: () => adminHomePage()),
         GetPage(name: '/users', page: () => UserApp()),
         GetPage(name: '/schools', page: () => SchoolsCrud()),
         GetPage(name: '/autorisations', page: () => AutorisationCrudPage()),
