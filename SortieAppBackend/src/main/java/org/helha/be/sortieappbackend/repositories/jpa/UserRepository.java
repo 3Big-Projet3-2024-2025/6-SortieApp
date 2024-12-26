3627a2c42ab85c81d653afb07f02197af9a82824
hus0w <la226628@student.helha.be>
2024-12-26 15:38:00 +0100

    Creating of '/getUsersBySchool' endpoint

--- SortieAppBackend/src/main/java/org/helha/be/sortieappbackend/repositories/jpa/UserRepository.java
+++ SortieAppBackend/src/main/java/org/helha/be/sortieappbackend/repositories/jpa/UserRepository.java
@@ -1,8 +1,11 @@
 package org.helha.be.sortieappbackend.repositories.jpa;
 
+import org.helha.be.sortieappbackend.models.School;
 import org.helha.be.sortieappbackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {}
